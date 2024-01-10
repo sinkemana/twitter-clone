05f0f7460798c3546413350fa762789fc6c99e65
utsav bhattarai <ubhattarai05@gmail.com>
2024-01-10 10:22:47 +0545

    added feed in the project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    # Customize the path to your feed or any other desired page
+    feeds_path
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_08_130503) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_09_155322) do
+  create_table "feeds", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
